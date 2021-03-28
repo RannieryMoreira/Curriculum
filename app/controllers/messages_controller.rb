d5150caaa537c1ddc2758a6e9dcc157602b73f8e
Ranniery Moreira <moreiraranni@gmail.com>
2021-03-28 13:31:07 -0300

    Translate error messages

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -29,7 +29,7 @@ class MessagesController < ApplicationController
 
     respond_to do |format|
       if @message.save
-        format.html { redirect_to @message, notice: "Message was successfully created." }
+        format.html { redirect_to @message, notice: "A mensagem foi criada com sucesso." }
         format.json { render :show, status: :created, location: @message }
       else
         format.html { render :new, status: :unprocessable_entity }
@@ -42,7 +42,7 @@ class MessagesController < ApplicationController
   def update
     respond_to do |format|
       if @message.update(message_params)
-        format.html { redirect_to @message, notice: "Message was successfully updated." }
+        format.html { redirect_to @message, notice: "A mensagem foi editada com sucesso." }
         format.json { render :show, status: :ok, location: @message }
       else
         format.html { render :edit, status: :unprocessable_entity }
@@ -55,7 +55,7 @@ class MessagesController < ApplicationController
   def destroy
     @message.destroy
     respond_to do |format|
-      format.html { redirect_to messages_url, notice: "Message was successfully destroyed." }
+      format.html { redirect_to messages_url, notice: "A mensagem foi deletada com sucesso." }
       format.json { head :no_content }
     end
   end
